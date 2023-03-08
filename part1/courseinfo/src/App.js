308180c9bc4633b449c4c2f0c4910694110d2137
ShadowZxu <shadowziyou@gmail.com>
2023-03-08 10:45:15 -0800

    add exercises 1.3 course information step3
    What:
    1. Modify the variable definitions of part1, part2 and part3 as objects

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,10 +1,7 @@
-const Header = () => {
-  // const-definition
-  const COURSE_TITLE = 'Half Stack application development'
-  
+const Header = (props) => {
   return (
     <>
-      <h1>{COURSE_TITLE}</h1>
+      <h1>{props.course}</h1>
     </>
   )
 }
@@ -12,7 +9,7 @@ const Header = () => {
 const Part = (props) => {
   return (
     <>
-      <p>{props.part} {props.exercises}</p>    
+      <p>{props.name} {props.exercises}</p>    
     </>
   )
 }
@@ -20,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part = {props.info.part1} exercises = {props.info.exercises1} />
-      <Part part = {props.info.part2} exercises = {props.info.exercises2} />
-      <Part part = {props.info.part3} exercises = {props.info.exercises3} />
+      <Part name = {props.part1.name} exercises = {props.part1.exercises} />
+      <Part name = {props.part2.name} exercises = {props.part2.exercises} />
+      <Part name = {props.part3.name} exercises = {props.part3.exercises} />
     </>
   )
 }
@@ -37,21 +34,26 @@ const Total = (props) => {
 
 
 const App = () => {
-  // const-definition
-  const INFO = {
-    part1: 'Fundamentals of React',
-    exercises1: 10,
-    part2: 'Using props to pass data',
-    exercises2: 7,
-    part3: 'State of a component',
-    exercises3: 14
+  // const-definition  
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
   }
 
   return (
     <>
-      <Header />
-      <Content info = {INFO} />
-      <Total count = {INFO.exercises1 + INFO.exercises2 + INFO.exercises3} />
+      <Header course = {course}/>
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} />
+      <Total count = {part1.exercises + part2.exercises + part3.exercises} />
     </>
   )
 }
