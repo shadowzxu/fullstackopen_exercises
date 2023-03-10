ecb3d11a45aa1f7d3f9a88149757af78aafcc504
ShadowZxu <shadowziyou@gmail.com>
2023-03-09 16:12:27 -0800

    add exercises 1.7 unicafe step2
    What:
    1.add more statistics about the gathered feedback: total, average and positive rate

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,13 +8,17 @@ const Button = (props) => {
   )
 }
 
-const Display = (props) => (<p>{props.text} {props.count}</p>)
+const Display = (props) => (<p>{props.text} {props.count} {props.symbol}</p>)
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad;
+  const average = (all === 0) ? 0 : (good - bad) / all;
+  const positive = (all === 0) ? 0 : (good / all) * 100;
 
   const addGoodCount = () => setGood(good + 1)
   const addNeutralCount = () => setNeutral(neutral + 1)
@@ -30,6 +34,9 @@ const App = () => {
       <Display text = "good" count = {good} />
       <Display text = "neutral" count = {neutral} />
       <Display text = "bad" count = {bad} />
+      <Display text = "all" count = {all} />
+      <Display text = "average" count = {average} />
+      <Display text = "positive" count = {positive} symbol = {"%"}/>
     </>
   )
 }
