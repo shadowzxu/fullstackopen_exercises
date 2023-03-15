aa48949efc078a812c0645bfd3c419d416bf85cf
ShadowZxu <shadowziyou@gmail.com>
2023-03-15 02:43:13 -0700

    exercises 2.7-2.8: The phonebook step2-3
    What:
    1. alert the user when adding names that already exist in the phonebook.
    2. allowing users to add phone numbers to the phone book.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,24 +2,41 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }, { name: 'Brto Hellas' }, { name: 'Crto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
-  const [newName, setNewName] = useState('a new name...')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault();
     console.log('button clicked', event.target);
 
+    if(persons.map((person) => person.name).find(name => name === newName.trim())){
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    
     const person = {
-      name: newName
+      name: newName.trim(),
+      number: newNumber.trim(),
+      id: persons.length + 1
     }
     setPersons(persons.concat(person));
     setNewName('');
+    setNewNumber('');
   }
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    // console.log(event.target.value);
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -28,14 +45,17 @@ const App = () => {
       {/* <div>debug: {newName}</div> */}
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange = {handleNameChange}/>
+          name: <input id = {newName} value={newName} onChange = {handleNameChange}/>
+        </div>
+        <div>
+          number: <input id = {newNumber} value={newNumber} onChange = {handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key = {person.name}>{person.name}</p>)}
+      {persons.map((person) => <p key = {person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
