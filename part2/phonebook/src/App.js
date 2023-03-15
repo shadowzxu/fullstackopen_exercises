f7719effd2dd29d80167363b590b2944a668de98
ShadowZxu <shadowziyou@gmail.com>
2023-03-15 01:43:40 -0700

    exercises 2.6: The phonebook step1
    What:
    1. implementing the addition of a person to the phonebook.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }, { name: 'Brto Hellas' }, { name: 'Crto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('a new name...')
+
+  const addName = (event) => {
+    event.preventDefault();
+    console.log('button clicked', event.target);
+
+    const person = {
+      name: newName
+    }
+    setPersons(persons.concat(person));
+    setNewName('');
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {/* <div>debug: {newName}</div> */}
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange = {handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <p key = {person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
