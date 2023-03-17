9f05cda729b15c30567e47b8b380fc512e66e9dc
ShadowZxu <shadowziyou@gmail.com>
2023-03-17 12:31:20 -0700

    2.11: The Phonebook Step6
    What:
    1. Modify the application such that the initial state of the data is fetched from the server using the axios.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,27 @@
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  //fetch the initial state of persons data from json-server
+  useEffect(() => {
+    // console.log('effect');
+
+    const eventHandler = response => {
+      // console.log('promise fullfilled');
+      setPersons(response.data);
+    }
+
+    const promise = axios.get('http://localhost:3001/persons');
+    promise.then(eventHandler)
+  }, [])
 
   const addName = (event) => {
     event.preventDefault();
@@ -47,6 +56,8 @@ const App = () => {
     // console.log(event.target.value);
     setNewFilter(event.target.value);    
   }
+
+
 
   const fillteredPersons = newFilter ? 
     persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase())) : persons;
