6589c795bdf5fe3bf941efd7a90769ed2eac0c9d
ShadowZxu <shadowziyou@gmail.com>
2023-03-23 13:03:10 -0700

    2.18* Data for countries, step1
    What:
    1. Create an application, in which one can look at data from various countries.
    2. When there is only one country matching the query, show the basic data of the country.

---
+++ part2/countries/src/App.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Display = ({ countries }) => {
+  
+  if(countries.length === 1){
+    const country = countries[0];
+    return (
+      <div>
+        <h3>{country.name}</h3>
+        <p>capital: {country.capital}</p>
+        <p>area: {country.area}</p>
+        <ul>
+          {Object.values(country.languages)
+            .map((language) => <li key = {language}>{language}</li>)}
+        </ul>
+        <img src={country.image} alt=''></img>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+        {countries.map((country) => 
+            <p key = {country.name}> 
+                {country.name}
+            </p>)
+        }
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [newFilter, setNewFilter] = useState('')
+  const [countries, setCountries] = useState(null)
+
+  useEffect(()=> {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data.map(country => {
+          return {
+            name: country.name.common,
+            languages: country.languages,
+            capital: country.capital,
+            area: country.area,
+            image: country.flags.png
+          }}))
+      })
+  },[])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);    
+  }
+
+  const filteredCountries = newFilter ? 
+    countries.filter((country) => country.name.toLowerCase().includes(newFilter.toLowerCase())) : [];
+
+  return (
+    <div>
+      <div>find countries: <input value={newFilter} onChange = {handleFilterChange}/></div>
+      <Display countries={filteredCountries} />
+    </div>
+  )
+}
+
+export default App
