9f12241e89ddde5a0a9bbe47d58ab8cf2443bf0a
ShadowZxu <shadowziyou@gmail.com>
2023-03-23 20:28:45 -0700

    2.20*: Data for countries, step3
    What:
    Add to the view showing the data of a single country, the weather report for the capital of that country.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Display = ({ countries, handleShowButtonClick }) => {
+const Display = ({ countries, weather, handleShowButtonClick }) => {
   
   if(countries.length === 1){
     const country = countries[0];
+
     return (
       <div>
         <h3>{country.name}</h3>
@@ -15,6 +16,11 @@ const Display = ({ countries, handleShowButtonClick }) => {
             .map((language) => <li key = {language}>{language}</li>)}
         </ul>
         <img src={country.image} alt=''></img>
+        
+        <h3>Weather in {weather.name}</h3>
+        <p>temperature: {weather.temp} Celcius</p>
+        <img src={weather.icon}></img>
+        <p>wind speed: {weather.wind} m/s</p>
       </div>
     )
   }
@@ -34,6 +40,7 @@ const App = () => {
 
   const [newFilter, setNewFilter] = useState('')
   const [countries, setCountries] = useState(null)
+  const [weather, setWeather] = useState({name: "", temp: 0, wind: 0})
 
   useEffect(()=> {
     axios.get('https://restcountries.com/v3.1/all')
@@ -49,6 +56,25 @@ const App = () => {
       })
   },[])
 
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY;
+    
+    if(newFilter){
+      const filteredCountries = countries.filter((country) => country.name.toLowerCase().includes(newFilter.toLowerCase()));
+      const capital = filteredCountries.length > 0 ? filteredCountries[0].capital[0] : "";
+      if(filteredCountries.length === 1 && (!weather || weather.name !== capital)){
+        console.log('get weather for: ', capital)
+        axios
+          .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`)
+          .then(response => setWeather({
+            name: response.data.name,
+            temp: Math.round((response.data.main.temp - 273.15) * 100) / 100,
+            wind: response.data.wind.speed,
+            icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+          }))
+      }}
+  }, [newFilter])
+
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value);    
   }
@@ -63,7 +89,7 @@ const App = () => {
   return (
     <div>
       <div>find countries: <input value={newFilter} onChange = {handleFilterChange}/></div>
-      <Display countries={filteredCountries} handleShowButtonClick={showCountry}/>
+      <Display countries={filteredCountries} weather = {weather} handleShowButtonClick={showCountry}/>
     </div>
   )
 }
