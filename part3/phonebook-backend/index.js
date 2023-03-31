17927daeb65fd9b45ef77c9ddc7d156484416f68
ShadowZxu <shadowziyou@gmail.com>
2023-03-31 02:54:42 -0700

    3.18*: Phonebook database step6
    What:
    1. update the handling of the api/persons/:id and info routes to use the database.
    2. update PUT API: error message for content missing.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -21,8 +21,12 @@ app.use(morgan(':method :url :status :response-time ms - :type'))
 
 let persons = []
 
-app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people<br>${Date()}</p>`)
+app.get('/info', (request, response, next) => {
+    Person.find()
+    .then(persons => {
+        response.send(`<p>Phonebook has info for ${persons.length} people<br>${Date()}</p>`)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
@@ -31,16 +35,17 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if(person){
-        response.json(person)
-    }
-    else{
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person){
+                response.json(person)
+            }
+            else{
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -73,6 +78,12 @@ app.post('/api/persons', (request, response) => {
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
+    if(!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
     const person = {
         name: body.name,
         number: body.number,
@@ -91,7 +102,6 @@ const unknownEndpoint = (request, response) => {
 
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint)
-
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
