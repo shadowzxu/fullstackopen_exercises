500c789abdff489f2f8330d981983246f160ef67
ShadowZxu <shadowziyou@gmail.com>
2023-04-05 00:18:11 -0700

    4.5*: helper functions and unit tests, step3
    What:
    1. implement favoriteBlog finds out which blog has the most likes.
    2. write appropriate unit test
    3. move mockup blog lists outside of the describe block.

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -9,7 +9,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) return {}
+
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite}, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
