55b0e2c186118341c958910491506712301e882f
ShadowZxu <shadowziyou@gmail.com>
2023-04-08 22:50:44 -0700

    Refactor blog_api_test.js and test_helper.js
    What:
    
    1. Refactor blogs api test for a more readable and scalable code structure.

--- part4/blog-list-backend/tests/blog_api.test.js
+++ part4/blog-list-backend/tests/blog_api.test.js
@@ -5,122 +5,110 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5
-  },
-]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  await Blog.insertMany(initialBlogs)
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('GET /api/blogs: get all blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const title = response.body.map(r => r.title)
+    expect(title).toContain(
+      'Go To Statement Considered Harmful'
+    )
+  })
+
+  test('the id property is existed', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
+describe('POST /api/blogs: add new blogs', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12
+    }
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(initialBlogs.length)
+    const response = await api.get('/api/blogs')
+
+    const title = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(title).toContain(
+      newBlog.title
+    )
+  })
+
+  test('new added blog has 0 likes if likes property is missing', async () => {
+    const newBlog = {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const result = response.body[response.body.length - 1].likes
+    expect(result).toBe(0)
+  })
+
+  test('expect return bad request if title or url property is missing', async () => {
+    let newBlog = {
+      title: 'title1',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    newBlog = {
+      author: 'author1',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-
-  const title = response.body.map(r => r.title)
-  expect(title).toContain(
-    'Go To Statement Considered Harmful'
-  )
-})
-
-test('the id property is existed', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body[0].id).toBeDefined()
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-
-  const title = response.body.map(r => r.title)
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(title).toContain(
-    newBlog.title
-  )
-})
-
-test('new added blog has 0 likes if likes property is missing', async () => {
-  const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-  const result = response.body[response.body.length - 1].likes
-  expect(result).toBe(0)
-})
-
-test('expect return bad request if title or url property is missing', async () => {
-  let newBlog = {
-    title: 'title1',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
-
-  newBlog = {
-    author: 'author1',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
-})
-
-describe('deletion of a blog', () => {
+describe('DELETE /api/blogs: deletion of a blog', () => {
   test('succeeds woth status code 204 if id is valid', async () => {
     const blogAtStart = await helper.blogsInDb()
     const blogToDelete = blogAtStart[0]
@@ -132,7 +120,7 @@ describe('deletion of a blog', () => {
     const blogsAtEnd = await helper.blogsInDb()
 
     expect(blogsAtEnd).toHaveLength(
-      initialBlogs.length -  1
+      helper.initialBlogs.length -  1
     )
 
     const titles = blogsAtEnd.map(t => t.title)
@@ -140,7 +128,7 @@ describe('deletion of a blog', () => {
   })
 })
 
-describe('update of a blog', () => {
+describe('PUT /api/blogs: update of a blog', () => {
   let blogToUpdate
   beforeEach(async () => {
     blogToUpdate = new Blog({
