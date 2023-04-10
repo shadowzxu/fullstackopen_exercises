f04b2821db0bd9e7afaae1575c8e242ed219b32e
ShadowZxu <shadowziyou@gmail.com>
2023-04-10 02:57:07 -0700

    4.15: bloglist expansion, step3
    What:
    
    1. Implement a way to create new users by doing an HTTP POST request to address api/users. Users have a username, password and name.
    2. Implement a way to see the details of all users by doing a suitable HTTP request.

--- part4/blog-list-backend/tests/test_helper.js
+++ part4/blog-list-backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -51,12 +52,32 @@ const initialBlogs = [
   }
 ]
 
+const initialUsers = [
+  {
+    'username': 'hellas',
+    'name': 'Arto Hellas',
+    'password': 'salainen'
+  },
+  {
+    'username': 'shadowzxu',
+    'name': 'Jason Xu',
+    'password': 'salainen'
+  },
+]
+
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 module.exports = {
   initialBlogs,
-  blogsInDb
+  initialUsers,
+  blogsInDb,
+  usersInDb
 }
---
+++ part4/blog-list-backend/tests/user_api.test.js
@@ -0,0 +1,100 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+
+describe('POST /api/users: when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+})
+
+describe('GET: /api/users: get all users', () => {
+  beforeEach( async () => {
+    await User.deleteMany({})
+    await User.insertMany(helper.initialUsers)
+  })
+
+  test('users are returned as json', async () => {
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all users are returned', async () => {
+    const response = await api.get('/api/users')
+
+    expect(response.body).toHaveLength(helper.initialUsers.length)
+  })
+
+  test('a specific user is within the returned users', async () => {
+    const response = await api.get('/api/users')
+
+    const username = response.body.map(r => r.username)
+    expect(username).toContain(
+      'hellas'
+    )
+  })
+
+  test('the id property is existed', async () => {
+    const response = await api.get('/api/users')
+
+    expect(response.body[0].id).toBeDefined()
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
