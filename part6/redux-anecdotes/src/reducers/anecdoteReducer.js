41decabf1d3bcb13bb2eb46037b7cdf19ecd8620
ShadowZxu <shadowziyou@gmail.com>
2023-05-01 14:47:17 -0700

    6.5: anecdotes, step3
    Make sure that the anecdotes are ordered by the number of votes.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -30,7 +30,9 @@ const reducer = (state = initialState, action) => {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1
       }
-      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote) 
+      return state
+              .map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+              .sort((a, b) => b.votes - a.votes)
     default:
       return state
   }
