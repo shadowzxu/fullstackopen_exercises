a210082e5032fa42ddd547398bd3a475d67a8438
ShadowZxu <shadowziyou@gmail.com>
2023-05-03 16:01:10 -0700

    6.9 Better anecdotes, step7
    Implement filtering for the anecdotes that are displayed to the user.

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    const sFilter = filter.trim().toLowerCase()
+    return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(sFilter))
+  })
+
   const dispatch = useDispatch()
 
 	return (
