d2d1bd5010eb289d3bb992cdbe25d36b83039fc0
ShadowZxu <shadowziyou@gmail.com>
2023-05-05 16:37:36 -0700

    6.17 Anecdotes and the backend, step4
    Modify the creation of a new anecdote to happen using asynchronous action creators, made possible by the Redux Thunk library.

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { notificationChange } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -17,8 +16,7 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
     showNotification(content)
   }  
 
