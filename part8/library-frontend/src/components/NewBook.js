f3227b277a124d0450a21ffb0dbd9cd54c66a8e0
ShadowZxu <shadowziyou@gmail.com>
2023-06-28 13:37:31 -0700

    8.10: Adding a book
    What:
    1. Implement adding new books.
    2. Update the cache to make sure that the Authors and Books views are kept up to date after a new book is added.

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
+import { useMutation } from '@apollo/client'
 import { useState } from 'react'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from './queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,14 +9,29 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_AUTHORS }, { query: ALL_BOOKS }]
+  })
+
   if (!props.show) {
     return null
   }
+  
 
   const submit = async (event) => {
     event.preventDefault()
 
     console.log('add book...')
+    
+    createBook({ 
+      variables: {
+        title: title, 
+        author: author,
+        published: Number(published),
+        genres: genres
+      }
+
+    })
 
     setTitle('')
     setPublished('')
