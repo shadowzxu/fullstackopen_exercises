d022046e2a556dd8000cd1df0f9426601c04b1f9
ShadowZxu <shadowziyou@gmail.com>
2023-06-30 15:55:13 -0700

    8.14: Database, part 2
    What:
    1. Fix all queries(to get allBooks working with the parameter author and bookCount field of an author object is not required) and mutations work.)
    2. move data to separate file.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -21,98 +21,6 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
 /*
   you can remove the placeholder query once your first own has been implemented 
 */
@@ -123,6 +31,11 @@ const typeDefs = `
     born: Int
     id: ID!
     bookCount: Int!
+  }
+
+  input AuthorInput {
+    name: String!
+    born: Int
   }
 
   type Book {
@@ -143,70 +56,66 @@ const typeDefs = `
   type Mutation {
     addBook(
       title: String!
-      author: String!
+      author: AuthorInput!
       published: Int!
       genres: [String]!
     ): Book
     
     editAuthor(
       name: String!
-      setBornTo: Int!
+      born: Int!
     ): Author
   }
 `
 
 const resolvers = {
-  Book: {
-    title: (root) => root.title,
-    published: (root) => root.published,
-    author: (root) => root.author,
-    genres: (root) => root.genres,
-    id: (root) => root.id
-  },
   Author: {
     name: (root) => root.name,
     born: (root) => root.born,
     id: (root) => root.id,
-    bookCount: (root) => books.filter(b => b.author === root.name).length
+    bookCount: async (root) => {
+      const author = await Author.findOne({ name: root.name })
+      return (await Book.find({ author: author })).length
+    }
   },
   Query: {
-    authorCount: () => authors.length,
-    bookCount: () => books.length,
-    allBooks: (root, args) => {
-      let booksForReturn = books
+    authorCount: () => Author.collection.countDocuments(),
+    bookCount: () => Book.collection.countDocuments(),
+    allAuthors: async (root, args) => {
+      return Author.find({})
+    },
+    allBooks: async (root, args) => {
+      let booksForReturn = await Book.find({}).populate('author')
       
       if(args.author) {
-        booksForReturn = booksForReturn.filter(b => b.author === args.author)
+        booksForReturn = booksForReturn.filter(b => b.author.name === args.author)
       }
       if(args.genre) {
         booksForReturn = booksForReturn.filter(b => b.genres.includes(args.genre))
       }
 
       return booksForReturn
-    },
-    allAuthors: () => authors
+    }
   },
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid()}
-      books = books.concat(book)
+    addBook: async (root, args) => {
+      let foundAuthor = await Author.findOne({ name: args.author.name })
 
-      if(!authors.find(author => author.name === args.author)){
-        author = { name: args.author, id: uuid()}
-        authors = authors.concat(author)
+      if(!foundAuthor) {
+        foundAuthor = new Author({ name: args.author.name, born: args.author.born })
+        await foundAuthor.save()
       }
 
-      return book
+      const book = new Book({ ...args, author: foundAuthor })
+      return book.save()
     },
-    editAuthor: (root, args) => {
-      const author = authors.find(a => a.name === args.name)
-      if(!author){
+    editAuthor: async (root, args) => {
+      const authorToUpdate = await Author.findOne({ name: args.name })
+      if(!authorToUpdate){
         return null
       }
-
-      const updatedAuthor = { ...author, born: args.setBornTo}
-      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
-      return updatedAuthor
+      authorToUpdate.born = args.born
+      return authorToUpdate.save()
     }
   }
 }
