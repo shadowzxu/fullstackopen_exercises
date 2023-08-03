8b7eca21dceaabcaa6e2b191e578b253b7b01f14
ShadowZxu <shadowziyou@gmail.com>
2023-08-03 10:28:50 -0700

    9.11: Patientor backend, step4
    What:
    Create data type Patient and set up the GET endpoint /api/patients which returns all patients to the frontend, excluding field ssn using utility type.

--- part9/patientor-backend/types.ts
+++ part9/patientor-backend/types.ts
@@ -1,5 +1,18 @@
+export type Gender = 'male' | 'female' | 'other';
+
 export interface Diagnose {
   code: string;
   name: string;
   latin?: string;
-}
+};
+
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: Gender;
+  occupation: string;
+};
+
+export type NonSensitivePatientEntry = Omit<Patient, 'ssn'>;
