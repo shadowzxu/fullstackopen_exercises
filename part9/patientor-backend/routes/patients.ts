315f95765ae611ced5f2a6e8017407a3df1c834a
ShadowZxu <shadowziyou@gmail.com>
2023-08-05 12:46:24 -0700

    9.12: Patientor backend, step5
    What:
    1. create POST endpoint /api/patients for adding patients.
    2. add parameters parsers and type guards for outside data sources.

--- part9/patientor-backend/routes/patients.ts
+++ part9/patientor-backend/routes/patients.ts
@@ -5,6 +5,21 @@ const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.send(patientsService.getNonSensitiveEntries());
-})
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newPatientEntry = patientsService.toNewPatientEntry(req.body);
+
+    const addedEntry = patientsService.addPatient(newPatientEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage: string = 'Something went wrong';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+});
 
 export default router;
--- part9/patientor-backend/services/patientsService.ts
+++ part9/patientor-backend/services/patientsService.ts
@@ -1,9 +1,67 @@
-import patients from "../data/patients"
-import { Patient, NonSensitivePatientEntry } from "../types";
+import { v1 as uuid } from 'uuid';
+import patients from "../data/patients";
+import { Patient, NonSensitivePatientEntry, NewPatientEntry, Gender } from "../types";
 
+/**
+ * type guards
+ */
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const isGender = (str: string): str is Gender => {
+  return ['male', 'female', 'other'].includes(str);
+};
+
+/**
+ * type parsers
+ */
+const parseName = (name: unknown): string => {
+  if(!name || !isString(name)) {
+    throw new Error('Incorrect or missing name');
+
+  }
+  return name;
+};
+
+const parseDateOfBirth = (dateOfBirth: unknown): string => {
+  if(!dateOfBirth || !isString(dateOfBirth) || !isDate(dateOfBirth)) {
+    throw new Error('Incorrect or missing date: ' + dateOfBirth);
+  }
+  return dateOfBirth;
+};
+
+const parseSsn = (ssn: unknown): string => {
+  if(!ssn || !isString(ssn)){
+    throw new Error('Incorrect or missing ssn: ' + ssn);
+  }
+  return ssn;
+};
+
+const parseOccupation = (occupation: unknown): string => {
+  if(!occupation || !isString(occupation)){
+    throw new Error('Incorrect or missing occupation: ' + occupation);
+  }
+  return occupation;
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if(!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender: ' + gender);
+  }
+  return gender;
+};
+
+/**
+ * Patient Services
+ */
 const getEntry = (): Patient[] => {
   return patients;
-}
+};
 
 const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
   return patients.map(({id, name, dateOfBirth, gender, occupation }) => ({
@@ -13,9 +71,34 @@ const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
     gender,
     occupation
   }));
-}
+};
+
+const addPatient = ( entry: NewPatientEntry ): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...entry
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
+const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+  console.log(object);
+  const newEntry: NewPatientEntry = {
+    name: parseName('Jason Xu'),
+    dateOfBirth: parseDateOfBirth('1996-11-10'),
+    ssn: parseSsn('333-666-999'),
+    occupation: parseOccupation('Developer'),
+    gender: parseGender('male')
+  };
+
+  return newEntry;
+};
 
 export default {
   getEntry,
-  getNonSensitiveEntries
+  getNonSensitiveEntries,
+  addPatient,
+  toNewPatientEntry
 }
