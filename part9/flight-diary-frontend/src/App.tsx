fb48264fa50b46d715bf420ceece401d9920e90e
ShadowZxu <shadowziyou@gmail.com>
2023-08-22 15:34:43 -0700

    Exercise 9.17
    What:
    add new diary entries from the frontend. Assuming that the user enters the data in a correct form.

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,20 +1,51 @@
 import Content from "./components/Content";
 import { DiaryEntry } from "./types";
 import { useEffect, useState } from "react";
-import { getAllDiaries } from "./services/diaryService";
+import { createDiary, getAllDiaries } from "./services/diaryService";
 
 function App() {
 
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+  const [date, setDate] = useState("2023-02-02");
+  const [visibility, setVisibility] = useState("great");
+  const [weather, setWeather] = useState("sunny");
+  const [comment, setComment] = useState("nice flight but a shaky landing.");
 
   useEffect(() => {
     getAllDiaries().then(data => {
       setDiaries(data);
     })
   }, [])
+
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    createDiary({ date, visibility, weather, comment }).then(data =>{
+      setDiaries(diaries.concat(data))
+    });
+
+    setDate('');
+    setVisibility('');
+    setWeather('');
+    setComment('');
+  }
   
   return (
     <div>
+      <h2>Add new entry</h2>
+      <form onSubmit={diaryCreation}>
+        <label>date: </label>
+        <input value={date} onChange={(event) => setDate(event.target.value)} /><br/><br/>
+        <label>visibility: </label>
+        <input value={visibility} onChange={(event) => setVisibility(event.target.value)} /><br/><br/>
+        <label>weather: </label>
+        <input value={weather} onChange={(event) => setWeather(event.target.value)} /><br/><br/>
+        <label>comment: </label>
+        <input value={comment} onChange={(event) => setComment(event.target.value)} /><br/><br/>
+        <button type='submit'>add</button>
+      </form>
+
       <h2>Diary Entries</h2>
       <Content diaries={diaries}/>
     </div>
--- part9/flight-diary-frontend/src/services/diaryService.tsx
+++ part9/flight-diary-frontend/src/services/diaryService.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL } from "../constants";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NewDiary } from "../types";
 
 const baseURL = `${API_BASE_URL}/diaries`
 
@@ -9,3 +9,9 @@ export const getAllDiaries = () => {
     .get<DiaryEntry[]>(baseURL)
     .then(response => response.data);
 };
+
+export const createDiary = (object: NewDiary) => {
+  return axios
+    .post<DiaryEntry>(baseURL, object)
+    .then(response => response.data);
+}
